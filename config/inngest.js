57334239b66efbfb37f8f210c66e2252f4390391
Saurav <saurv123@outlook.com>
2025-09-14 18:37:08 +0530

    as

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest, fn } from "inngest";
 import connectDB from "./db";
-import User from "./models/User";
+import User from "@/models/user";
 
 // Initialize Inngest client
 export const inngest = new Inngest({ id: "power-electronics" });
