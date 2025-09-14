1e6050b378d00d9f51105d93ccdd1964100eb525
Saurav <saurv123@outlook.com>
2025-09-14 18:42:43 +0530

    AS

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   imageUrl: { type: String, required: true },
-  cartItems : { type: object , default: {} },
+  cartItems : { type: Object , default: {} },
 } , { minimize: false   });
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
