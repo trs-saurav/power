e60829c71a2cef4b47160dfd19dc02890447d23e
Saurav <saurv123@outlook.com>
2025-09-21 21:56:20 +0530

    seo-1

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    _id: { type: String, required: true },
+    _id: { type: String, required: true,  },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   imageUrl: { type: String, required: true },
